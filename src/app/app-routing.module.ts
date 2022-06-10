e5c9d644d1529f148e722280c8d394a94d7a87bd
William Cheung <william.cheung.c@gmail.com>
2022-06-10 13:59:33 +0100

    add page-not-found - "ng generate m page-not-found --route=404 --routing=true --module=app" add app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'cameras', loadChildren: () => import('./birdcams/birdcams.module').then(m => m.BirdcamsModule) }];
+const routes: Routes = [
+  {
+    path: 'birdcams',
+    loadChildren: () => import('./birdcams/birdcams.module').then(m => m.BirdcamsModule)
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'birdcams',
+  },
+  {
+    path: '404',
+    loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
